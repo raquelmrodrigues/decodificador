5bc64d0b2809b956277a99f2ed71114b1a3be455
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-04-03 19:07:53 -0300

    esconder e mostrar campo

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function criaCampo() {
-    
-}
+var seletor = document.getElementById('seletor')
+
+seletor.addEventListener('change', function() {
+    if(seletor.value == "Cifra de César"){
+        document.getElementById('elementoEscondido').style.display = "block";
+    } else {
+        document.getElementById('elementoEscondido').style.display = "none";
+    }
+});
+
